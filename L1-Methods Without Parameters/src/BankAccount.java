da832dfd7ff8c35876987bc3c32e682e74c975f4
bustobri000 <bustobri000@warren.k12.in.us>
2021-09-10 13:00:14 -0400

    L1-L2 Work finished.

--- L1-Methods Without Parameters/src/BankAccount.java
+++ L1-Methods Without Parameters/src/BankAccount.java
@@ -1,8 +1,13 @@
+import javax.swing.*;
+
 public class BankAccount {
 
     //create a program which simulates a bank account
 
     public static void main(String[] args) {
+
+        deposit();
+        withdrawal();
 
     }
 
@@ -10,9 +15,26 @@ public class BankAccount {
     Prompt the user to enter their balance and the deposit amount,
     then display the new balance. */
 
+    public static void deposit(){
+
+        double balance = Double.parseDouble(JOptionPane.showInputDialog("What is your current balance?"));
+        double deposit = Double.parseDouble(JOptionPane.showInputDialog("What is your deposit amount?"));
+
+        balance += deposit;
+        JOptionPane.showMessageDialog(null, "Your new balance is : " + balance);
+    }
 
     /*write a method to simulate a withdrawal.
     Prompt the user to enter their balance the withdrawal amount,
     then display the new balance.
      */
+
+    public static  void withdrawal(){
+
+        double balance = Double.parseDouble(JOptionPane.showInputDialog("What is your current balance?"));
+        double withdrawal = Double.parseDouble(JOptionPane.showInputDialog("What is your withdraw amount?"));
+
+        balance -= withdrawal;
+        JOptionPane.showMessageDialog(null, "Your new balance is : " + balance);
+    }
 }
--- L2-Methods with Parameters/src/LemonadeStand.java
+++ L2-Methods with Parameters/src/LemonadeStand.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+import java.text.DecimalFormat;
 public class LemonadeStand {
 
     /*
@@ -16,7 +18,23 @@ public class LemonadeStand {
      */
 
     public static void main(String[] args) {
-
+        int cupsSixteen = Integer.parseInt(JOptionPane.showInputDialog("What is the amount of 16 oz cups sold?"));
+        int cupsTwenty = Integer.parseInt(JOptionPane.showInputDialog("What is the amount of 20 oz cups? sold"));
+        sales(cupsSixteen, cupsTwenty);
     }
 
+    public static void sales(int cupsSixteen, int cupsTwenty) {
+        DecimalFormat round = new DecimalFormat("#,###.00");
+        double sixteenSales = cupsSixteen * 0.5;
+        double twentySales = cupsTwenty * 0.99;
+        double tax = 7.5;
+        double total = (sixteenSales + twentySales) * (1 + tax/100.0);
+
+        String message = "Your Order is:";
+        message += "\nNumber of 16oz cups sold: " + cupsSixteen;
+        message += "\nNumber of 20oz cups sold: " + cupsTwenty;
+        message += "\nThe total sales is: $" + round.format(total);
+
+        JOptionPane.showMessageDialog(null, message);
+    }
 }
